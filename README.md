fbc821305e109786434f97da2f41a9b6376e5aa6
vl-tk <vlad.work.39@ya.ru>
2023-05-25 21:23:19 +0200

    working version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pyToJson
-Sublime plugin for converting python printed dictionary to JSON.
+### PyDict to JSON
+
+Sublime Text 4 plugin for converting python printed dictionary to JSON.
+
+### Installation
+
+cd ~/.config/sublime-text/Packages/
+
+git clone https://github.com/vl-tk/pydict_to_json.git
+
+### Usage
+
+1) select printed dict
+
+2) Command Palette (Ctrl+Shift+P) - type command name and apply it to text
+
+### Uninstallation
+
+Use Command Pallette - Uninstall Package
+
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import sublime
-import sublime_plugin
 import json
+from collections import OrderedDict
+
+import sublime_plugin
 
 
-class pytojsonCommand(sublime_plugin.TextCommand):
-	""" converts python dict to json and beautifies it.
-	"""
-	def run(self, edit):
-		for selection in self.view.sel():
-			text = self.view.substr(selection)
-			try:
-				text = eval(text)
-				if type(text) == dict:
-					_dict = dict(text)
-				elif type(text) == list:
-					_dict = text
-				replace_text = json.dumps(_dict, sort_keys=True, indent=4)
-			except Exception as e:
-				print(e)
-			else:
-				self.view.replace(edit, selection, replace_text)
+class PydictToJsonCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            text = self.view.substr(selection)
+            try:
+                text = eval(text)
+                if type(text) == dict:
+                    _dict = OrderedDict(text)
+                elif type(text) == list:
+                    _dict = text
+                replace_text = json.dumps(_dict, indent=4)
+            except Exception as e:
+                print(e)
+            else:
+                self.view.replace(edit, selection, replace_text)
